582ad5de07c778e52a40223f1d3eac3df2e1888a
nikalsh <niklas@helander.se>
2019-02-20 13:52:43 +0100

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# northwind-spa
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+``` Deployment
+
+Build your vue code using npm run build command. This will create a folder dist which should have index.html file and static folder.
+Copy dist folder into your server code repository.
+
+//These 2 lines make sure that vue and express app are coming from the same server. 
+      app.use('/static', express.static(path.join(__dirname,"../public/dist/static/"))); 
+      app.get('/', function(req,res) {
+         res.sendFile('index.html', { root: path.join(__dirname, '../public/dist/') });
+      });
---
+++ spa/src/main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import Hello from './components/HelloWorld'
+
+import About from './components/About'
+
+
+const routes = [
+  //define the root url of the application.
+  { path: '/', component: Hello },
+  { path: '/about', component: About }
+  ]
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
+})
+//instatinat the vnue instance
+new Vue({
+//define the selector for the root component
+  el: '#app',
+  //pass the template to the root component
+  template: '<App/>',
+  //declare components that the root component can access
+  components: { App },
+  //pass in the router to the Vue instance
+  router
+}).$mount('#app')//mount the router on the app
