1a58dd44986262ce438318e5dcf6cef44e5ec6ee
nikalsh <Niklas.Helander@yh.nackademin.se>
2019-03-12 16:27:28 +0100

    new component OrderForm

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,30 @@
 </template>
 
 <script>
-  import NavBar from './components/NavBar'
-  export default {
-    name: 'app',
-    components: {
-      'NavBar': NavBar,
-    }
+import NavBar from "./components/NavBar";
+export default {
+  name: "app",
+  components: {
+    NavBar: NavBar
   }
+};
 </script>
 <!-- styling for the component -->
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    text-decoration-color: aliceblue;
-    color: #2c3e50;
-    bottom: 0;
-    background-color: #34495e;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top:0;
-  }
+#app {
+  margin: 0;
+  padding: 0;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  text-decoration-color: aliceblue;
+  color: #2c3e50;
+  bottom: 0;
+  background-color: #34495e;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
 </style>
